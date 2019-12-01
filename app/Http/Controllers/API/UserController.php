8aafe7c0083e69788cf0a187f3ccb45ba409208a
Joel Kibona <joelvankibona@gmail.com>
2019-12-01 17:43:03 +0300

    added server side validation on user controller

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -27,13 +27,20 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request,[
+            'name'=> 'required|string|max:191',
+            'email'=> 'required|string|email|max:191|unique:users',
+            'password'=> 'required|string|min:6',
+            'type'=> 'required',
+        ]);
+
         return User::create([
-            'name'=> $request['name'],
-            'email'=> $request['email'],
-            'password'=> Hash::make($request['password']),
-            'type'=> $request['type'],
-            'bio'=>$request['bio'],
-            'photo'=>$request['photo'],
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password']),
+            'type' => $request['type'],
+            'bio' => $request['bio'],
+            'photo' => $request['photo'],
         ]);
     }
 
